b66fc70aeb3b8ac4d6c76d5f52cf015472e6316a
abdiabdulle <abdi_abdulle@yahoo.com>
2020-10-29 15:47:11 -0700

    updates

--- src/screens/Bill.js
+++ src/screens/Bill.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import CommonStyles from '../CommonStyles';
-import { View, Text, StyleSheet, TouchableHighlight, TouchableOpacity, Dimensions, FlatList } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList,Alert,AsyncStorage } from 'react-native';
 import { Icon, Input } from 'native-base';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
+import axios from 'axios';
+import { ViewUtils } from '../Utils';
+import database from '@react-native-firebase/database';
+import auth from '@react-native-firebase/auth';
+import Loader from '.././assets/components/Loader';
+
 
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -12,17 +18,101 @@ class Bill extends React.Component {
         super(props);
 
         this.state = {
-            // bill: props.route.params.res.data,
-            //             net_amount:0
-            bill: []
+            isLoading: false,
+            user: {},
+            allBills: props.route.params.res.data,
+            myBill:[],
+            billId: '',
+            net_amount: 0,
+            emails:[],
+            myBill: []
         }
 
-        // console.warn("my billll -- ",this.state.bill)
-        this.state.bill = ['11', '33', '44', '55']
+    }   
+
+    async _getBill(){
+        const billId =  await AsyncStorage.getItem('billId')
+        this.setState({billId:billId})        
+        const bill = this.state.allBills.filter(x => x._id == this.state.billId)
+        Object.keys(bill).forEach(key => {
+         this.setState({myBill:bill[key].foodItem})
+         this.setState({net_amount:bill[key].amount})
+       });
     }
+
+    componentDidMount() {
+        this._getBill()
+        auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ user: user })
+
+                database()
+                    .ref(`/Groups/${this.state.user.uid}/Friends`)
+                    .once("value")
+                    .then(async snapshot => {
+                        let emails = [];
+                       // console.warn("this.state.user.email ::: ",this.state.user.email)
+                        emails.push(this.state.user.email)
+                        snapshot.forEach(item => {
+
+                            const temp = item.val();
+                            emails.push(temp.email);
+                        })
+                       // console.warn("emails ::: ",emails)
+
+                        this.setState({ emails: emails });
+                    });
+
+            }
+        });
+    }
+
+    showAlertForShare() {
+        Alert.alert(
+            "Share Bill",
+            `Do you want to share bill with your friends?`,
+            [
+                {
+                    text: "No",
+                    onPress: () => console.log("Cancel Pressed"),
+                },
+                { text: "Yes", onPress: () => this.shareBill() }
+            ],
+            { cancelable: false }
+        );
+    }
+
+    async shareBill() {
+        var self = this;
+      //  self.setState({ isLoading: true })
+        // console.warn('dsadsads :: ',this.state.emails);
+
+        axios.post('https://checkoutapp1.herokuapp.com/api/noti', {
+            userIds: this.state.emails,
+            billId: this.state.billId,
+            isDivide: true
+        })
+            .then(function (response) {
+                console.warn('res :: ',response);
+                self.props.navigation.navigate('Payment')
+               // self.setState({ loading: false })      
+            })
+            .catch(function (error) {
+                console.warn('err :: ',error);
+
+               // self.setState({ loading: false })
+            });
+
+    }
+
+
     render() {
-        //this.state.net_amount = this.state.bill[0].totalprice;
-        // console.warn("bill --- ",this.state.bill);
+        
+      
+        //console.warn("ss :: ",myBill[0])
+        // this.state.myBill = myBill[0].foodItem
+        // this.state.net_amount = myBill[0].amount
+
         return (
             <View style={Style.container}>
                 <KeyboardAwareScrollView style={Style.container}>
@@ -47,11 +137,9 @@ class Bill extends React.Component {
                     <View style={{ margin: 10 }}>
                         <View>
                             <View style={{ flexDirection: 'row' }}>
-                                <Text style={{ flex: 1, fontSize: 16 }}>Food</Text>
+                                <Text style={{ flex: 1, fontSize: 20 }}>Item</Text>
                                 <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-between' }}>
-                                    <Text style={Style.TextStyle}>Q</Text>
-                                    <Text style={Style.TextStyle}>Unit</Text>
-                                    <Text style={Style.TextStyle}>Total</Text>
+                                    <Text style={Style.TextStyle}>Price</Text>
                                 </View>
 
                             </View>
@@ -59,18 +147,13 @@ class Bill extends React.Component {
                     </View>
                     <View style={{ marginLeft: 10, marginRight: 10 }}>
                         <FlatList
-                            data={this.state.bill}
+                            data={this.state.myBill}
                             renderItem={({ item }) => (
                                 <View>
                                     <View style={{ flexDirection: 'row', }}>
-                                        <Text style={{ flex: 1, fontSize: 16 }}>{item.name}</Text>
+                                        <Text style={{ flex: 1, fontSize: 16,color:'#8BC080' }}>{item.item}</Text>
                                         <View style={{ flexDirection: 'row', flex: 1, justifyContent: 'space-between' }}>
-                                            {/* <Text style={Style.TextStyle}>{item.quantity}</Text>
-                                            <Text style={Style.TextStyle}>${item.price}</Text>
-                                            <Text style={Style.TextStyle}>${item.TOTAL}</Text> */}
-                                            <Text style={Style.TextStyle}>2</Text>
-                                            <Text style={Style.TextStyle}>100</Text>
-                                            <Text style={Style.TextStyle}>200</Text>
+                                            <Text style={{fontSize: 16,color:'#8BC080'}}>${item.price}</Text>
                                         </View>
 
                                     </View>
@@ -78,23 +161,30 @@ class Bill extends React.Component {
                             )}
                         />
                     </View>
-                    <View style={{ margin: 40 }}>
+                    <View style={{ marginTop: 40, marginLeft: 10 }}>
                         <View>
-                            <View style={{ flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 24 }}>Net Amount = </Text>
-                                {/* <Text style={{ fontSize: 24 }}>${this.state.net_amount}</Text> */}
-                                <Text style={{ fontSize: 24 }}>800</Text>
-
+                            <View style={{ flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-end',marginRight:10 }}>
+                            <Text style={{ fontSize: 20,color:'#8BC080' }}>Total = ${this.state.net_amount}</Text>
                             </View>
                         </View>
                     </View>
-                    <View style={Style.btnContainer}>
-                        <TouchableOpacity style={Style.btnStyle}
-                            onPress={()=>this.props.navigation.navigate('Payment')}
-                        >
-                            <Text style={Style.btnText}>Payment</Text>
-                        </TouchableOpacity>
+                    <View style={{ flex: 1, justifyContent: 'flex-end', }}>
+                        <View style={Style.btnContainer}>
+                            <TouchableOpacity style={Style.btnStyle}
+                                onPress={() => this.props.navigation.navigate('Payment')}
+                            >
+                                <Text style={Style.btnText}>Pay Full Bill</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={Style.btnContainer2}>
+                            <TouchableOpacity style={Style.btnStyle}
+                                onPress={() => this.showAlertForShare()}
+                            >
+                                <Text style={Style.btnText}>Share Bill</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
+                    <Loader loading={this.state.isLoading} />
                 </KeyboardAwareScrollView>
             </View>
         )
@@ -134,8 +224,8 @@ const Style = StyleSheet.create({
         marginLeft: 10
     },
     TextStyle: {
-        color: '#8BC080',
-        fontSize: 16,
+        color: 'black',
+        fontSize: 20,
         textAlign: 'center',
 
     },
@@ -144,18 +234,22 @@ const Style = StyleSheet.create({
         color: '#8BC080',
         marginLeft: 10
     }, btnContainer: {
-        width: '70%',
-        marginTop: 70,
+        width: '95%',
+        marginTop: 30,
+        alignSelf: 'center',
+    },
+    btnContainer2: {
+        width: '95%',
         alignSelf: 'center',
     },
     btnStyle: {
         marginVertical: 10,
-        backgroundColor: '#F5F5F5',
+        backgroundColor: '#8BC080',
         borderRadius: 5
     },
     btnText: {
         textAlign: 'center',
-        color: '#8BC080',
+        color: '#FFF',
         fontSize: 20,
         padding: 10,
         fontWeight: 'bold'
